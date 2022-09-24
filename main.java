55a65a903d5c754d56ad6d113b9bb97b5e1104ad
AAJOrgealvarado <jaalvarado1@libertadores.edu.co>
2022-09-24 07:28:11 -0500

    se modifico con el nombre de la clase

--- main.java
+++ main.java
@@ -1,13 +1,12 @@
+public class Main{
 public static void main(String[] args){ 
 
  System.out.println("Hola");
 
  Multi ejemplo=new Multi();
 
- 
-
  System.out.println(ejemplo.multiplicacion(4,1));
 
  }
-
 }
+
