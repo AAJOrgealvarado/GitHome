75e45c44c2ce617ad6f89bdeef8fb0c97ccb2e9c
AAJOrgealvarado <jaalvarado1@libertadores.edu.co>
2022-10-15 11:11:35 -0500

    Agrgamos rama bot

--- main.java
+++ main.java
@@ -4,8 +4,11 @@ public static void main(String[] args){
  System.out.println("Hola");
 
  Multi ejemplo=new Multi();
-
+ Prueba_unitaria prueba= new Prueba_unitaria();
  System.out.println(ejemplo.multiplicacion(4,2));
+ System.out.println(prueba.prueba(ejemplo,4,2,8));
+
+
 
  }
 }
